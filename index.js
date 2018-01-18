6bec8c89ef857413ee415b0e50de5899ae5be2b1
Anne van Gorkom <anne@vangorkom.com>
2018-01-18 11:49:24 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -77,5 +77,17 @@ function removeFromCart(item) {
 }
 
 function placeOrder(cardNumber) {
-  // write your code here
+  if (cardNumber.length === 0) {
+    return console.console.log("Sorry, we don't have a credit card on file for you..");
+  }
 }
+
+//The placeOrder() function accepts one argument, a credit card number.
+
+//If no argument is received, the function should print out
+    //Sorry, we don't have a credit card on file for you..
+
+//If a card number is received, the function should print out
+    //Your total cost is $71, which will be charged to the card 83296759.
+          //(where 71 is the value returned by total() and 83296759 is the credit card number passed to placeOrder()).
+    //Then, it should empty the cart array.
