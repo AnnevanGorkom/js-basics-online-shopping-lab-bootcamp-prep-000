11abae03ab5d4d7651900ec888a4a70ffde132ad
Anne van Gorkom <anne@vangorkom.com>
2018-01-18 11:40:31 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -58,7 +58,17 @@ function total() {
 }
 
 function removeFromCart(item) {
+  itemInCart = false;
+
+  for (var i = 0; i < cart.length; i++) {
+    if (cart[i].hasOwnProperty(item)) {
+      itemInCart = true;
+      cart = cart.slice(0, i).concat(cart.slice(i + 1));
+      cart.length--;
+    }
+  }
   
+  return cart;
 }
 
 function placeOrder(cardNumber) {
