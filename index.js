e3e8734d96bd2bde73f0b5101efc379eb14458f0
Anne van Gorkom <anne@vangorkom.com>
2018-01-18 11:50:18 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -77,7 +77,7 @@ function removeFromCart(item) {
 }
 
 function placeOrder(cardNumber) {
-  if (cardNumber === 0) {
+  if (cardNumber === undefined) {
     return console.console.log("Sorry, we don't have a credit card on file for you..");
   }
 }
