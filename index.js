655130c3365d7a29f4b00d24b9992b596d746175
Anne van Gorkom <anne@vangorkom.com>
2018-01-18 11:43:21 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -61,11 +61,11 @@ function removeFromCart(item) {
   var itemInCart = false;
   var lang = cart.length;
 
-  for (var i = 0; i < cart.length; i++) {
+  for (var i = 0; i < lang; i++) {
     if (cart[i].hasOwnProperty(item)) {
       itemInCart = true;
       cart = cart.slice(0, i).concat(cart.slice(i + 1));
-      cart.length--;
+      lang--;
     }
   }
 
